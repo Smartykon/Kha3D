9485ba5a4ce227a8e7d45fdbf17073abbf56774e
Martin Konrad <m2018@ktxsoftware.com>
2019-06-22 18:18:42 +0200

    Änderung bei Mesh::fromPly()

--- Sources/kha3d/Mesh.hx
+++ Sources/kha3d/Mesh.hx
@@ -1,5 +1,6 @@
 package kha3d;
 
+import kha.FastFloat;
 import kha.graphics4.VertexData;
 import kha.graphics4.VertexStructure;
 import kha.graphics4.Usage;
@@ -55,9 +56,14 @@ class Mesh {
 		return mesh;
 	}
 
-	public static function fromPly(data: PlyData): Mesh {
+	public static function fromPly(data: PlyData, xo: Int, yo: Int, scale: FastFloat): Mesh {
 		var mesh: Mesh = new Mesh();
 		mesh.id = currentId++;
+
+		// zo automatisch
+		var zo: Int = 2;
+		if ((xo == zo) || (yo == zo)) zo--;
+		if ((xo == zo) || (yo == zo)) zo--;
 
 		var vertices = data.va.coords;
 		//var normals = data.geometryObjects[0].mesh.vertexArrays[1].values;
@@ -72,14 +78,9 @@ class Mesh {
 		mesh.vertexBuffer = new VertexBuffer(vertices.length, mesh.structure, Usage.StaticUsage);
 		var buffer = mesh.vertexBuffer.lock();
 		for (i in 0...Std.int(vertices.length / 3)) {
-			//buffer.set(i * 8 + 0, vertices[i * 3 + 0]);
-			//buffer.set(i * 8 + 1, vertices[i * 3 + 1]);
-			//buffer.set(i * 8 + 2, vertices[i * 3 + 2]);
-
-			// Für MagicaVoxel: Koordinaten etwas umsortieren...
-			buffer.set(i * 8 + 0, vertices[i * 3 + 0]);
-			buffer.set(i * 8 + 1, vertices[i * 3 + 2]);
-			buffer.set(i * 8 + 2, vertices[i * 3 + 1]);
+			buffer.set(i * 8 + 0, vertices[i * 3 + xo] * scale);
+			buffer.set(i * 8 + 1, vertices[i * 3 + yo] * scale);
+			buffer.set(i * 8 + 2, vertices[i * 3 + zo] * scale);
 
 			buffer.set(i * 8 + 3, 0.0);//normals[i * 3 + 0]);
 			buffer.set(i * 8 + 4, 1.0);//normals[i * 3 + 1]);
